b07882e3548c34a8002b78e645e01074ccc91b22
RomagnoliDev <romagnolidevelopment@gmail.com>
2025-11-20 14:29:07 -0300

    fonts 2.b

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,17 @@
 import { Lugrasimo } from "next/font/google";
 import type { Metadata } from "next";
-import "./../styles/globals.css";
-
-export const metadata: Metadata = {
-  title: "Romagnoli Group",
-  description: "Café Romagnoli & Toscano Tabaco - Calidad y experiencia con raíz italiana.",
-};
+import "../styles/globals.css";
 
 const lugrasimo = Lugrasimo({
   subsets: ["latin"],
   weight: "400",
   variable: "--font-lugrasimo"
 });
+
+export const metadata: Metadata = {
+  title: "Romagnoli Group",
+  description: "Café Romagnoli & Toscano Tabaco - Calidad y experiencia con raíz italiana.",
+};
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
